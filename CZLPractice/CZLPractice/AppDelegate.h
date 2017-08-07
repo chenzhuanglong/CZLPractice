44ba66a1a6a050d789abc2ce383a6496378543f6
壮龙 <chenzhuanglong@5315.qq.com>
2017-08-07 10:43:44 +0800

    更新

---
+++ CZLPractice/CZLPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CZLPractice
+//
+//  Created by yuzeux on 2017/8/7.
+//  Copyright © 2017年 DerenChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
