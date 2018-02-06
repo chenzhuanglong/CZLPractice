4fa02b7332f225dbcc72aed96a051e18281e932e
壮龙 <chenzhuanglong@5315.qq.com>
2018-02-06 21:31:21 +0800

    更新

---
+++ ZLSPAR/FaceIt/FaceViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by yuzeux on 2018/2/1.
+//  Copyright © 2018年 晶石. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ZLSPAR/ZLSPAR/Classer/Emotions/ZLEmotionsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ZLEmotionsViewController.swift
+//  ZLSPAR
+//
+//  Created by yuzeux on 2018/2/1.
+//  Copyright © 2018年 晶石. All rights reserved.
+//
+
+import UIKit
+
+class ZLEmotionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     
+    }
+    
+
+}
